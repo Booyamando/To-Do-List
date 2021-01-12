a1e61e8a8c5ae4801a3d08a4bbe74204e9fb2450
Booyamando <filibs10@gmail.com>
2021-01-12 22:55:33 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [list, setlist] = useState([
+		{ label: "walk the dog", done: false },
+		{ label: "walk the dog", done: false }
+	]);
+
+	const [todo, setTodo] = useState("");
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	const handleKeyPress = e => {
+		if (e.key === "Enter") {
+			setlist(
+				list.concat([
+					{
+						label: todo,
+						done: false
+					}
+				])
+			);
+			setTodo("");
+		}
+	};
+
+	const deleteToDo = index => {
+		setlist(list.filter((item, i) => i !== index));
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>
+				<h1>To Do</h1>
+				<form onSubmit={handleSubmit}>
+					<ul className="list-unstyled">
+						<li>
+							{""}
+							<input
+								value={todo}
+								type="text"
+								placeholder="What do you need?"
+								onChange={e => setTodo(e.target.value)}
+								onKeyPress={e => handleKeyPress(e)}
+							/>
+						</li>
+						{list.map((item, index) => {
+							return (
+								<li key={index}>
+									{item.label}{" "}
+									<span onClick={() => deleteToDo(index)}>
+										<FontAwesomeIcon icon={faTrash} />
+									</span>
+								</li>
+							);
+						})}
+						<li>
+							{list.length} item
+							{list.length > 1 || list.length === 0
+								? "s"
+								: null}{" "}
+							left
+						</li>
+					</ul>
+				</form>
+			</div>
 		</div>
 	);
 }
